9f107cc8c44f0fcd1827d0f1238179b1f1ec5095
mohamedRajab0 <elzimmohamed@gmail.com>
2024-08-11 22:38:47 +0300

    frist commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,10 @@
+def pascal_triangle(n):
+    res = [[1]]
+    
+    for i in range(n - 1):
+        temp = [0] + res[-1] + [0]
+        row = []
+        for j in range(len(res[-1]) + 1):
+            row.append(temp[j] + temp[j + 1])
+        res.append(row)
+    return res
