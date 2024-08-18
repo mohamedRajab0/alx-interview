4c4cac318338a304c09bfcca94fd366ab874d70c
mohamedRajab0 <elzimmohamed@gmail.com>
2024-08-18 17:07:11 +0300

    add doc

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+""" method that determines if all the boxes can be opened."""
+
 
 def canUnlockAll(boxes):
+    """-
+    Args:
+        boxes: list of boxes
+    Return:
+        true if no lock box, else false"""
     unlock_boxes = set([0])
 
     keyes = boxes[0].copy()
